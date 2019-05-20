19b09b15043f9e87a6540067df8301c9fa71fe27
Uppercod <mtrujillo@outlook.cl>
2019-05-20 02:51:27 -0400

    add the wait of 3 frames to issue the loading

--- src/index.js
+++ src/index.js
@@ -9,6 +9,8 @@ export function lazy(callback) {
 	return ({ loading, ...props }) => {
 		let state = useImport(callback, props);
 
+		if (state.wait) return;
+
 		return state.loading
 			? loading
 			: typeof state.default == "function"
@@ -22,18 +24,22 @@ export function lazy(callback) {
  * @param {object} [props]
  */
 export function useImport(callback, props) {
-	let [state, setState] = useState({ loading: true });
+	let [state, setState] = useState({ wait: true });
 	useEffect(() => {
 		let loading = true;
-		callback(props).then(
-			md =>
-				loading &&
-				setState({
-					loading: false,
-					...md
-				})
-		);
+		callback(props).then(md => {
+			loading && setState(md);
+
+			loading = false;
+		});
+		fps(() => {
+			loading && setState({ loading: true });
+		});
 		return () => (loading = false);
 	}, [callback]);
 	return state;
+}
+
+function fps(callback, count = 3) {
+	count-- ? requestAnimationFrame(() => fps(callback, count)) : callback();
 }
